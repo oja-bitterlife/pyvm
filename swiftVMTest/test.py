a8b1543aac5df6363195d961707631567fba2988
ozaki <bitter.life.club@gmail.com>
2026-08-03 16:51:37 +0900

    テストを複数の比較に対応

--- swiftVMTest/test.py
+++ swiftVMTest/test.py
@@ -1,25 +1,29 @@
 from assets.vm import *
 
+value = 0
+
 
 def main():
     VM[VM_EVENT] = EVENT_KEY_START
     VM[VM_SELECT_NO] = 2
 
-    if VM[VM_EVENT] == EVENT_KEY_A or VM[VM_EVENT] == EVENT_KEY_START:
-        match VM[VM_SELECT_NO]:
-            case 0:
-                return 1
-            case 1:
-                return 2
-            case _:
-                VM[VM_FREE] = 0
-                while VM[VM_FREE] < 3:
-                    VM[VM_FREE] = VM[VM_FREE] + 1
-                VM[VM_SELECT_NO] = VM[VM_SELECT_NO] + VM[VM_FREE]
-                return -1
-
     VM[VM_FREE] = 0
-    for VM[VM_FREE] in [3, 4, EVENT_KEY_B]:
-        VM[VM_SELECT_NO] = VM[VM_SELECT_NO] + VM[VM_FREE]
+    while VM[VM_FREE] < 3:
+        VM[VM_FREE] = VM[VM_FREE] + 1
 
-    return VM[VM_SELECT_NO]
+    VM[VM_FREE] = -VM[VM_FREE]
+    VM[VM_FREE] += 2
+
+    if VM[VM_EVENT] == EVENT_KEY_A or VM[VM_EVENT] == EVENT_KEY_START:
+        if 1 < 2 < 3 < 4:
+            match VM[VM_SELECT_NO]:
+                case 2 | 3:
+                    return VM[VM_FREE]
+                case value:
+                    return value
+        return 0
+
+    if not (VM[VM_FREE] == 0):
+        return VM[VM_FREE]
+
+    return 0
