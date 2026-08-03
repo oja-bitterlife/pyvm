ea3cfcfa158cd421eb79dc4f81f8d35922ff6af4
ozaki <bitter.life.club@gmail.com>
2026-08-03 16:55:57 +0900

    演算子のカバレッジ向上

--- swiftVMTest/test.py
+++ swiftVMTest/test.py
@@ -14,11 +14,18 @@ def main():
     VM[VM_FREE] = -VM[VM_FREE]
     VM[VM_FREE] += 2
 
+    VM[VM_FREE] = ((VM[VM_FREE] & 2) | 3) ^ 1
+    VM[VM_FREE] = (VM[VM_FREE] + 2) - 1
+    VM[VM_FREE] = (VM[VM_FREE] * 3) / 2
+    VM[VM_FREE] = (VM[VM_FREE] % 2) + 5
+
     if VM[VM_EVENT] == EVENT_KEY_A or VM[VM_EVENT] == EVENT_KEY_START:
         if 1 < 2 < 3 < 4:
             match VM[VM_SELECT_NO]:
                 case 2 | 3:
-                    return VM[VM_FREE]
+                    if 1 == 1 and 2 != 3 and 1 < 2 and 1 <= 2 and 2 > 1 and 2 >= 2:
+                        return VM[VM_FREE]
+                    return 0
                 case value:
                     return value
         return 0
